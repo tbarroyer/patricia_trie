046adc2ef5e7d6d3f015314f7e3e64f4c6a20a60
thibaut <t.barroyer@me.com>
2017-07-31 23:41:43 +0200

    correct distance func

--- src/tools.cc
+++ src/tools.cc
@@ -6,26 +6,23 @@ namespace textMining
   {
     size_t n1 = s1.length();
     size_t n2 = s2.length();
+
     size_t d[n1 + 1][n2 + 1];
     for (size_t i = 0; i <= n1; ++i)
       d[i][0] = i;
     for (size_t i = 0; i <= n2; ++i)
       d[0][i] = i;
     int cost;
-    for (size_t i = 0; i <= n1; i++)
+    for (size_t i = 1; i <= n1; i++)
     {
-      for (size_t j = 0; j <= n2; j++)
+      for (size_t j = 1; j <= n2; j++)
       {
-        cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1 ;
-        if ( (i > 1) && (j > 1) && (s1[i] == s2[j - 1]) && (s1[i - 1] == s2[j]))
+        cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+        d[i][j] = std::min(std::min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + cost);
+
+        if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1])
         {
-          size_t a = std::min(d[i - 1][j], d[i][j - 1] + 1);
-          size_t b = std::min(d[i][j] + cost, d[i - 2][j - 2]);
-          d[i][j] = std::min(a, b);
-        }
-        else
-        {
-          d[i][j] = std::min(std::min(d[i][j -1] + 1, d[i - 1][j] + 1), d[i - 1][j - 1] + cost);
+          d[i][j] = std::min(d[i][j], d[i - 2][j - 2] + cost);
         }
       }
     }
